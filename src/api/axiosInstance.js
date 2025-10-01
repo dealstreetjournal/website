a2e48c6067494319af76ea679b4caa34b81e2ada
Prabhat Rajput <prabhatsingh7557@gmail.com>
2025-10-01 12:11:18 +0530

    update code

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const API_BASE_URL = 'https://web.dealstreetjournal.com'
+// const API_BASE_URL = 'http://localhost:8081'
+
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -8,3 +10,5 @@ const axiosInstance = axios.create({
 })
 
 export default axiosInstance
+
+// git add karn eke liye use kiya hu ye comment
