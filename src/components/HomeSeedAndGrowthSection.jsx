f8da033b95ab50bc063f038726033dd3bad39be3
Saurav Singh <sauravsingh382001@gmail.com>
2025-10-23 15:44:42 +0530

    update p tag

--- src/components/HomeSeedAndGrowthSection.jsx
+++ src/components/HomeSeedAndGrowthSection.jsx
@@ -37,10 +37,7 @@ const HomeSeedAndGrowthSection = ({ seedData, growthData }) => {
             </h1>
             {Seeds && Seeds.length > 0 && (
               <>
-                <Link
-                  to={`seed/${Seeds[0].id}`}
-                  className="relative mb-5 w-full"
-                >
+                <Link to={`seed/${Seeds[0].id}`} className="relative mb-5">
                   <div className="w-full rounded-md bg-white overflow-hidden">
                     <img
                       src={Seeds[0].imageUrl}
@@ -50,7 +47,7 @@ const HomeSeedAndGrowthSection = ({ seedData, growthData }) => {
                     />
                   </div>
                   <div className="absolute bottom-0 left-0 w-full h-22 bg-gradient-to-t from-slate-500/100 to-transparent rounded-b-md"></div>
-                  <p className="absolute bottom-1 left-0 w-full block box-border text-white font-aptos-regular line-clamp-2 px-2 z-10">
+                  <p className="absolute bottom-1 left-0 right-0 text-white font-aptos-regular line-clamp-2 px-2 z-10">
                     {Seeds[0]?.title}
                   </p>
                 </Link>
@@ -104,10 +101,7 @@ const HomeSeedAndGrowthSection = ({ seedData, growthData }) => {
             </h1>
             {growths && growths.length > 0 && (
               <>
-                <Link
-                  to={`growth/${growths[0].id}`}
-                  className="relative mb-5 w-full"
-                >
+                <Link to={`growth/${growths[0].id}`} className="relative mb-5">
                   <div className="w-full rounded-md bg-white">
                     <img
                       src={growths[0].imageUrl}
@@ -117,7 +111,7 @@ const HomeSeedAndGrowthSection = ({ seedData, growthData }) => {
                     />
                   </div>
                   <div className="absolute bottom-0 left-0 w-full h-22 bg-gradient-to-t from-gray-500/100 to-transparent rounded-b-md"></div>
-                  <p className="absolute bottom-1 left-0 w-full block box-border text-white font-aptos-regular line-clamp-2 px-2 z-10">
+                  <p className="absolute bottom-1 left-0 right-0 text-white font-aptos-regular line-clamp-2 px-2 z-10">
                     {growths[0]?.title}
                   </p>
                 </Link>
